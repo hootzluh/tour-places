11c030f5d437f2f4765e5065e7af2483c620654f
Justin Hutzler <hootzluh@gmail.com>
2023-04-05 21:12:20 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Navbar from './components/Navbar/Navbar';
+import Header from './components/Header';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
